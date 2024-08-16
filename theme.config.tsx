2faadd72bac0bae4adcdaac9de4e2ea108162493
Jhon <85789728+jhonswg@users.noreply.github.com>
2024-08-16 09:15:55 +0700

    Twitter

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,7 +16,7 @@ const config: DocsThemeConfig = {
   // Tambahkan konfigurasi untuk Twitter menggunakan navbar
   navbar: {
     extraContent: (
-      <a href="https://x.com/jhonswgeth" target="_blank" rel="noopener noreferrer">
+      <a href="https://x.com/jhonswgeth" target="_blank" rel="noopener noreferrer" className="nx-p-2 nx-text-current">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path fill="currentColor" d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
         </svg>
