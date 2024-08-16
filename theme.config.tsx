712ed0db202aa1826c131a055f2a585d7010a79a
Jhon <85789728+jhonswg@users.noreply.github.com>
2024-08-16 08:54:04 +0700

    Default

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,24 +5,9 @@ const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
-    icon: (
-      <>
-        <a href="https://github.com/shuding/nextra-docs-template" target="_blank" rel="noopener noreferrer">
-          <img src="/path-to-github-icon.svg" alt="GitHub" style={{ marginRight: 8 }} />
-        </a>
-        <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">
-          <img src="/logo/twitter.svg" alt="Twitter" style={{ marginRight: 8 }} />
-        </a>
-      </>
-    )
   },
   chat: {
     link: 'https://discord.com',
-    icon: (
-      <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-        <img src="/path-to-discord-icon.svg" alt="Discord" style={{ marginRight: 8 }} />
-      </a>
-    )
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
