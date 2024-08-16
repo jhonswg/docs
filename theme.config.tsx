2cd5b2cab29979eceefdfa7f23748e0ff8ef9da9
Jhon <85789728+jhonswg@users.noreply.github.com>
2024-08-16 08:35:49 +0700

    New

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,32 +5,29 @@ const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
+    icon: (
+      <>
+        <a href="https://github.com/shuding/nextra-docs-template" target="_blank" rel="noopener noreferrer">
+          <img src="/path-to-github-icon.svg" alt="GitHub" style={{ marginRight: 8 }} />
+        </a>
+        <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">
+          <img src="/path-to-twitter-icon.svg" alt="Twitter" style={{ marginRight: 8 }} />
+        </a>
+      </>
+    )
   },
   chat: {
     link: 'https://discord.com',
+    icon: (
+      <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+        <img src="/path-to-discord-icon.svg" alt="Discord" style={{ marginRight: 8 }} />
+      </a>
+    )
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
   },
-  // Tambahkan ini
-  navs: [
-    {
-      name: "GitHub",
-      url: "https://github.com/shuding/nextra-docs-template",
-      icon: <img src="/path-to-github-icon.svg" alt="GitHub" />
-    },
-    {
-      name: "Discord",
-      url: "https://discord.com",
-      icon: <img src="/path-to-discord-icon.svg" alt="Discord" />
-    },
-    {
-      name: "Twitter",
-      url: "https://twitter.com/yourtwitterhandle",
-      icon: <img src="/path-to-twitter-icon.svg" alt="Twitter" />
-    }
-  ],
 }
 
 export default config
