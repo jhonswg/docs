5c9289b59014b1174650b35fde9707a08ab1f953
Jhon <85789728+jhonswg@users.noreply.github.com>
2024-08-16 09:18:30 +0700

    Up

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/jhonswg',
+    link: 'https://github.com/jhonswg',
   },
   chat: {
     link: 'https://discord.com/jhonswg',
