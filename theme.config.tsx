75a113f3264d5049408df373edcc2f3ba8684f1a
Jhon <85789728+jhonswg@users.noreply.github.com>
2024-08-16 08:50:41 +0700

    Logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,10 +8,10 @@ const config: DocsThemeConfig = {
     icon: (
       <>
         <a href="https://github.com/shuding/nextra-docs-template" target="_blank" rel="noopener noreferrer">
-          <img src="https://www.svgrepo.com/show/361509/github-logo.svg" alt="GitHub" style={{ marginRight: 8 }} />
+          <img src="/path-to-github-icon.svg" alt="GitHub" style={{ marginRight: 8 }} />
         </a>
         <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">
-          <img src="https://www.svgrepo.com/show/303746/twitter-icon.svg" alt="Twitter" style={{ marginRight: 8 }} />
+          <img src="/logo/twitter.svg" alt="Twitter" style={{ marginRight: 8 }} />
         </a>
       </>
     )
@@ -20,7 +20,7 @@ const config: DocsThemeConfig = {
     link: 'https://discord.com',
     icon: (
       <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-        <img src="https://www.svgrepo.com/show/353655/discord-icon.svg" alt="Discord" style={{ marginRight: 8 }} />
+        <img src="/path-to-discord-icon.svg" alt="Discord" style={{ marginRight: 8 }} />
       </a>
     )
   },
